670bdf88390b2577d7240f1b2017c7b522c1db59
etcadinfinitum <lizzy.presland@gmail.com>
2017-07-17 00:05:07 +0000

    Cracking the Coding Interview Chapter 2, Question 5 Sum of Lists Question Text: You have two numbers represented by a linked list, where each node contains a single digit. These digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.

---
+++ src/SimpleNode.java
@@ -0,0 +1,12 @@
+
+public class SimpleNode {
+    
+    public SimpleNode next;
+    public int data;
+    
+    public SimpleNode(int data) {
+        this.data = data;
+        
+        
+    }
+}
