b6d0ef58ae70ee4a76dc1355e0573afb7e1b2257
etcadinfinitum <lizzy.presland@gmail.com>
2017-07-22 04:04:13 +0000

    Revised static method and added 1 additional test case to PSVM

--- src/Ch2Pr5_SumLists.java
+++ src/Ch2Pr5_SumLists.java
@@ -28,6 +28,23 @@ public class Ch2Pr5_SumLists {
         SimpleList test1 = sumLists(list1, list2);
         System.out.println("Test 1\nInitial List 1: " + list1.toString() + "\nInitial List 2: " + list2.toString());
         System.out.println("Result: " + test1.toString()); 
+                
+        // test 1
+        SimpleList list3 = new SimpleList();
+        list3.addToEnd(1);
+        list3.addToEnd(1);
+        list3.addToEnd(1);
+        
+        SimpleList list4 = new SimpleList();
+        list4.addToEnd(8);
+        list4.addToEnd(9);
+        list4.addToEnd(9);
+        list4.addToEnd(9);
+        list4.addToEnd(9);
+        
+        SimpleList test2 = sumLists(list3, list4);
+        System.out.println("Test 2\nInitial List 1: " + list3.toString() + "\nInitial List 2: " + list4.toString());
+        System.out.println("Result: " + test2.toString()); 
     }
     
     /**
@@ -43,23 +60,23 @@ public class Ch2Pr5_SumLists {
         int carry = 0;
         SimpleNode node1 = list1.front;
         SimpleNode node2 = list2.front;
-        boolean stop = false;
-        while (!stop) {
-            if (node1 == null && node2 == null) {
-                stop = true;
-            } else {
+        while (node1 != null || node2 != null || carry != 0) {
                 if (node1 != null) {
                     val1 = node1.data;
                     node1 = node1.next;
+                } else {
+                    val1 = 0;
                 }
                 if (node2 != null) {
                     val2 = node2.data;
                     node2 = node2.next;
+                } else {
+                    val2 = 0;
                 }
                 result.addToEnd((val1 + val2 + carry) % 10);
-                carry = (int)Math.floor((val1 + val2 + carry) / 10);
+                carry = (val1 + val2 + carry) / 10;
             }
-        }
+        
         return result;
     }    
 }
